f8d26181cf601eda91c53a51f00a4c002e557422
Jae Lee <jlee@yetitrails.com>
2017-04-06 18:07:50 +0100

    initial exercise

---
+++ exercise1.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+const r = require('ramda');
+const assert = require('assert');
+const zero = [' _ ',
+              '| |',
+              '|_|'];
+const one = ['   ',
+             '  |',
+             '  |'];
+const two = [' _ ',
+             ' _|',
+             '|_ '];
+const three = [' _ ',
+               ' _|',
+               ' _|'];
+const four = ['   ',
+              '|_|',
+              '  |'];
+const five = [' _ ',
+              '|_ ',
+              ' _|'];
+const six = [' _ ',
+             '|_ ',
+             '|_|'];
+const seven = [' _ ',
+               '  |',
+               '  |'];
+const eight = [' _ ',
+               '|_|',
+               '|_|'];
+const nine = [' _ ',
+              '|_|',
+              ' _|'];
+
+function chunk(n, array) {
+  return r.range(0, array.length/n).map(i => array.slice(i*n, i*n+n))
+}
+
+function toNumber(ledMatrix) {
+  return r.indexOf(ledMatrix, [zero, one, two, three, four, five, six, seven, eight, nine]);
+}
+
+var readLedMatrixLine = r.pipe(r.map(r.splitEvery(3)),
+                               r.transpose,
+                               r.map(toNumber),
+                               r.invoker(1, 'join')(""));
+
+fs.readFile('l33t.txt', 'utf8', function(err, input) {
+  fs.readFile('treasure.txt', 'utf8', function(err, expected) {
+    var output = r.pipe(r.partial(chunk, [4]),
+                        r.map(r.compose(readLedMatrixLine, r.dropLast(1))))(input.split('\n'));
+    assert.deepEqual(output, expected.split('\n'));
+  });
+});
