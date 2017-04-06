3ef9475070512da0dff98a1375a3f6fedd667295
Jae Lee <jlee@yetitrails.com>
2017-04-06 21:30:15 +0100

    use of r.join

--- exercise1.js
+++ exercise1.js
@@ -43,12 +43,14 @@ function toNumber(ledMatrix) {
 var readLedMatrixLine = r.pipe(r.map(r.splitEvery(3)),
                                r.transpose,
                                r.map(toNumber),
-                               r.invoker(1, 'join')(""));
+                               r.join(''));
+
+var readLedMatrixLines = r.pipe(r.partial(chunk, [4]),
+                                r.map(r.compose(readLedMatrixLine, r.dropLast(1))));
 
 fs.readFile('l33t.txt', 'utf8', function(err, input) {
   fs.readFile('treasure.txt', 'utf8', function(err, expected) {
-    var output = r.pipe(r.partial(chunk, [4]),
-                        r.map(r.compose(readLedMatrixLine, r.dropLast(1))))(input.split('\n'));
+    var output = readLedMatrixLines(input.split('\n'));
     assert.deepEqual(output, expected.split('\n'));
   });
 });
